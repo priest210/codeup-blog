96c9f41139e7fba035390b06c677a472446d6f9a
Ken Priest <ken@priest210.com>
2014-03-25 18:58:25 -0500

    Cleaned up routes Deleted comment in BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,11 +2,7 @@
 
 class BaseController extends Controller {
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
+
 	protected function setupLayout()
 	{
 		if ( ! is_null($this->layout))
--- app/routes.php
+++ app/routes.php
@@ -11,44 +11,17 @@
 |
 */
 
+Route::resource('posts', 'PostsController');
+
 Route::get('/', 'HomeController@showWelcome');
+
 Route::get('/resume', 'HomeController@showResume');
 
-// Route::get('/', function()
-// {
-// 	return View::make('hello');
-// });
+Route::get('/portfolio', 'HomeController@showPortfolio');
 
-Route::get('/', 'HomeController@showResume');
 
-Route::get('/ ', 'HomeController@showPortfolio');
 
-Route::get('/sayhello/{name}', function($name) 
-{
-	$data = array(
-		'name' => $name
-	);
-	return View::make('my-first-view')->with($data);
-});
 
-Route::get('/rolldice/{guess}', function($guess) {
-	
-	$roll = mt_rand(1, 6);
-
-	if ($guess == $roll) {
-		$result = "Great job your guess was correct!";
-
-	}	else {
-		$result = "Sorry wrong answer";
-	}
-
-	$data = array(
-		'result' => $result, 
-		'guess' => $guess, 
-		'roll' => $roll
-	);
-	return View::make('roll-dice')->with($data);
-});
 
 
 
