87c31f1a2167f993a4309a60ccfdeeca4b9eb178
Ken Priest <ken@priest210.com>
2014-03-26 17:22:23 -0500

    Refactored Navbar.

--- app/views/resume-view.blade.php
+++ app/views/resume-view.blade.php
@@ -1,139 +1,201 @@
-@extends('layouts.master')
+@extends('layouts.master-resume')
 
-
-@section('toolbar')
-    <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">Resume</a>
-          <a class="blog-nav-item" href="#">Portfolio</a>
-          <a class="blog-nav-item" href="#">About me</a>
-          <a class="blog-nav-item" href="#">Hire me</a>
-        </nav>
-      </div>
-    </div>
+@section('title')
+    <title>Ken Priest's Resume</title>
 @stop
 
-    <div class="container">
+@section('cssScript')
+    <link href="css/superhero-bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/superhero-bootstrap.css">
+    <link href="css/resume.css" rel="stylesheet">
+@stop
 
-      <div class="blog-header">
-        <h1 class="blog-title">The Blog of Ken Priest</h1>
-        <p class="lead blog-description">This is blog contains information about coding, technology and me!</p>
+@section('jumbo')
+	<h1>Ken Priest </h1>
+	<p>Welcome to my online resum&eacute;.
+	</p>  
+    <p>This page was created using the finest HTML, CSS  and Javascript craftsmanship.  
+    </p>
+	<p>Rest assured no shortcuts were taken and no animals were harmed during the development process.</p>
+@stop
+
+
+  <div class="navbar navbar-inverse  navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button> 
       </div>
 
-      <div class="row">
-
-        <div class="col-sm-8 blog-main">
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">Sample blog post</h2>
-            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-
-            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-            <hr>
-            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <h2>Heading</h2>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-            <pre><code>Example code block</code></pre>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>
-              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-              <li>Donec id elit non mi porta gravida at eget metus.</li>
-              <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-            <ol>
-              <li>Vestibulum id ligula porta felis euismod semper.</li>
-              <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-              <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-            </ol>
-            <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-          </div><!-- /.blog-post -->
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">Another blog post</h2>
-            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-
-            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          </div><!-- /.blog-post -->
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">New feature</h2>
-            <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>
-              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-              <li>Donec id elit non mi porta gravida at eget metus.</li>
-              <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-          </div><!-- /.blog-post -->
-
-          <ul class="pager">
-            <li><a href="#">Previous</a></li>
-            <li><a href="#">Next</a></li>
-          </ul>
-
-        </div><!-- /.blog-main -->
-
-        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-          <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-          <div class="sidebar-module">
-            <h4>Archives</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">January 2014</a></li>
-              <li><a href="#">December 2013</a></li>
-              <li><a href="#">November 2013</a></li>
-              <li><a href="#">October 2013</a></li>
-              <li><a href="#">September 2013</a></li>
-              <li><a href="#">August 2013</a></li>
-              <li><a href="#">July 2013</a></li>
-              <li><a href="#">June 2013</a></li>
-              <li><a href="#">May 2013</a></li>
-              <li><a href="#">April 2013</a></li>
-              <li><a href="#">March 2013</a></li>
-              <li><a href="#">February 2013</a></li>
-            </ol>
-          </div>
-          <div class="sidebar-module">
-            <h4>Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Linkedin</a></li>
-              <li><a href="#">Twitter</a></li>
-            </ol>
-          </div>
-        </div><!-- /.blog-sidebar -->
-
-      </div><!-- /.row -->
-
-    </div><!-- /.container -->
-
-    <div class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
+      <div class="navbar-collapse collapse navbar-inverse-collapse" role="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="#profile">Profile</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#contact">Contact me</a></li>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
+
+@section('content')
+
+	<div>
+		<div class="panel panel-danger">
+		  <div class="panel-heading">
+		    <h3 class="panel-title" id="profile">Profile</h3>
+		  </div>
+		  <div class="panel-body">
+		    <div class="well well-lg">Here is where I will add an objective/summary statement.</div>
+		  </div>
+		</div>
+
+		<div class="panel panel-primary">
+		  	<div class="panel-heading">
+		    <h3 class="panel-title" id="skills">Skills</h3>
+			</div>
+		</div>
+	</div>
+	
+	<div class="panel panel-primary">
+	    <div class="panel-body">
+	    <table class="table" id="skills-table">
+	          <tr>
+	              <td>Linux</td>
+	              <td>HTML</td> 
+	              <td>Bootstrap</td>
+	              <td>Project Management</td>
+	              <td>Test edge</td>
+	          </tr>
+	          <tr>
+	              <td>Apache</td>
+	              <td>CSS</td>
+	              <td>b</td>
+	              <td>2</td> 
+	              <td>v</td>
+	          </tr>
+	          <tr>
+	              <td>MySQL</td>
+	              <td>Laravel</td>
+	              <td>c</td>
+	              <td>3</td>
+	              <td>x</td>
+	          </tr>
+	          <tr>
+	              <td>PHP</td>
+	              <td>GIT</td> 
+	              <td>d</td>
+	              <td>4</td>
+	              <td>y</td>
+	          </tr>
+	          <tr>
+	              <td>Javascript</td>
+	              <td>GitHub</td>
+	              <td>e</td>
+	              <td>5</td> 
+	              <td>z</td>
+	          </tr>
+	    </table>  
+	    </div>
+	</div>
+	
+
+	<div class="panel panel-warning">
+	    <div class="panel-heading">
+	    <h3 class="panel-title" id="education">Education</h3>
+	  </div>
+	  <div class="panel-title"> 
+	    <a href="http://www.codeup.com" target="_blank"class="list-group-item">
+	    <i class="fa fa-laptop "></i> Codeup, Computer Programming Bootcamp,  Feb 2014 - Present
+	      </a>
+	  </div>
+	  <div class="panel-body"> 
+	    San Antonio, Texas
+	  </div>
+
+	  <div class="panel-title">
+	    <a href="http://www.uiw.edu/mha" target="_blank" class="list-group-item" >
+	    <i class="fa fa-pencil "></i> Master of Health Administration, GPA 3.93
+	      </a>
+	  </div>
+	  <div class="panel-body">
+	  University of the Incarnate Word, San Antonio, TX,  2010 – 2012
+	  </div>
+	  <div class="panel-title">
+	    <a href="http://www.wbu.edu" target="_blank" class="list-group-item">
+	    <i class="fa fa-pencil "></i> Bachelor, Business Administration, GPA 3.4
+	      </a>
+	  </div>
+	<div class="panel-body"> 
+	    Wayland Baptist University, San Antonio, TX,  2005 – 2010
+	</div>
+
+	</div>
+	<div class="panel panel-warning">
+	  <div id="experience-header" class="panel-heading" id="experience-header">
+	    <h3 class="panel-title"  id="experience">Experience</h3>
+	  </div>
+	  <div class="panel-body">
+	 
+	    <ul class="list-group">
+	      <h3>E&amp;P Wireline(Schlumberger)</h3>
+	      <h4>Lead/Arming Operator</h4>
+	          
+	        <li class="list-group-item">Responsible for entire scope of   wireleine operations at wellsite</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	    </ul>
+
+	    <ul class="list-group">
+	      <h3>Graduate Student (Full-time)</h3>
+	      <h4>Master of Health Administration</h4>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	    </ul>
+
+	    <ul class="list-group">
+	      <h3>Watermark Asset Management LLC.</h3>
+	      <h4>Owner/Project Manager</h4>  
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	    </ul>
+
+	    <ul class="list-group">
+	      <h3>Paradise Farms LLC.</h3>
+	      <h4>Operations &amp; Project Manager</h4> 
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	    </ul>
+
+	    <ul class="list-group">
+	      <h3>United States Air Force</h3>
+	      <h4>Law Office Manager/Court Administrator/Paralegal</h4> 
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	        <li class="list-group-item">Key responsbilbity</li>
+	    </ul>   
+	  </div>
+	</div>
+</div>
+@stop
+
+
+@section('footer')
 
 
     <!-- Bootstrap core JavaScript
@@ -142,6 +204,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <script src="../../assets/js/docs.min.js"></script>
-  </body>
-</html>
+@stop
 
