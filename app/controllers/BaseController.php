500781daa32ae3a590376d6c2441f702a0f8ffb0
Ken Priest <ken@priest210.com>
2014-03-27 19:12:45 -0500

    Removed white space.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -3,6 +3,13 @@
 class BaseController extends Controller {
 
 
+	public function __construct()
+	{
+	    // require csrf token for all post, delete, and put actions
+	    $this->beforeFilter('csrf', array('on' => array('post', 'delete', 'put')));
+		}
+
+
 	protected function setupLayout()
 	{
 		if ( ! is_null($this->layout))
