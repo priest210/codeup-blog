a49b4e9f5a653db5fd4da7d2e89955ba5a472cec
Ken Priest <ken@priest210.com>
2014-03-27 19:20:16 -0500

    Initial commit

---
+++ app/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master-blog')
+
+
+
+@section('content')
+	<!-- Iterates through posts returned from table in DB -->
+	@foreach ($posts as $post)
+	<div class="well well-lg">
+		<div class="blog-post">
+		    <h2 class="blog-post-title">{{{$post->title}}}</h2>
+            <p class="blog-post-meta">{{{$post->created_at}}} by Ken Priest</p>
+            <p>{{{$post->body}}}</p>
+            <hr>	            
+		</div>
+	</div>
+     @endforeach
+@stop
+
+
+
+
---
+++ app/views/posts/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master-blog')
+
+
+@section('content')
+	<h1>{{{ $posts->title }}}</h1>
+
+	<!-- Grabs single post from object returned from table in DB -->
+		<div class="blog-post">
+	            <h2 class="blog-post-title">{{{$posts->title}}}</h2>
+	            <p class="blog-post-meta">{{{$posts->created_at}}}</p>
+	            <p>{{{$posts->body}}}</p>
+	          	<a href="{{{ action('PostsController@index') }}}">Return to posts listing</a></p>
+		</div>
+@stop
