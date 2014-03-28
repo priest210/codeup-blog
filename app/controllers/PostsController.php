59ed56e96d56d37fedb5745d8e03d18e7344bfac
Ken Priest <ken@priest210.com>
2014-03-27 19:17:54 -0500

    Changed Update action to add validator

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,28 +2,37 @@
 
 class PostsController extends \BaseController {
 
-
 	public function index()
 	{
 		$posts = Post::all();
 		return View::make('posts.index')->with('posts', $posts);
 	}
 
-
 	public function create()
 	{
-		return View::make('posts.create');
+		return View::make('posts.create-edit')->with('post', new Post());
 	}
-
 
 	public function store()
 	{	
-		Log::info(Input::all());
-	 	$post = new Post();
-		$post->title = Input::get('title');
-		$post->body = Input::get('body');
-		$post->save();
-		return Redirect::action('PostsController@index');
+
+		    // create the validator
+		    $validator = Validator::make(Input::all(), Post::$rules);
+
+		    // attempt validation
+		    if ($validator->fails())
+		    {
+		        return Redirect::back()->withInput()->withErrors($validator);
+		    }
+		    else {
+				// save to DB
+				Log::info(Input::all());
+			 	$post = new Post();
+				$post->title = Input::get('title');
+				$post->body = Input::get('body');
+				$post->save();
+				return Redirect::action('PostsController@index');
+			}
 	}
 
 
@@ -36,13 +45,30 @@ class PostsController extends \BaseController {
 	
 	public function edit($id)
 	{
-		return "Show a form for editing a specific post";
+		$post = Post::findOrFail($id);
+		return View::make('posts.create-edit')->with('post', $post);
 	}
 
 	
 	public function update($id)
 	{
-		return "Update a specific post";
+			$post = Post::findOrFail($id);
+
+		  	// create the validator
+		    $validator = Validator::make(Input::all(), Post::$rules);
+
+		    // attempt validation
+		    if ($validator->fails())
+		    {
+		        return Redirect::back()->withInput()->withErrors($validator);
+		    }
+		    else {
+				// save to DB
+				$post->title = Input::get('title');
+				$post->body = Input::get('body');
+				$post->save();
+				return Redirect::action('PostsController@index');
+			}
 	}
 
 	
