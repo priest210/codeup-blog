d0e68949265b820e54d1b96d14750c11ad87e25a
Ken Priest <ken@priest210.com>
2014-03-31 10:42:55 -0500

    Added Accessor for create UTC date/time.

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon/Carbon;
+
 class Post extends Eloquent {
 
 	protected $table = 'posts';
@@ -8,4 +10,10 @@ class Post extends Eloquent {
 		'title' => 'required|max:100',
 		'body' => 'required|max:10000'
 	);
+}
+
+public function getCreatedAtAttribute($value)
+{
+    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+    return $utc->setTimezone('America/Chicago');
 }
--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,7 +7,7 @@
 	<!-- Grabs single post from object returned from table in DB -->
 		<div class="blog-post">
 	            <h2 class="blog-post-title">{{{$posts->title}}}</h2>
-	            <p class="blog-post-meta">{{{$posts->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}}}</p>
+	            <p class="blog-post-meta">{{{$posts->created_at->format('l, F jS Y @ h:i:s A')}}}</p>
 	            <p>{{{$posts->body}}}</p>
 	          	<a href="{{{ action('PostsController@index') }}}">Return to posts listing</a></p>
 		</div>
