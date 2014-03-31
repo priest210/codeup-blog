a607e662a8b5b50d709a610d56814f1f020ccf10
Ken Priest <ken@priest210.com>
2014-03-31 16:47:55 -0500

    Refactored code to implement Base Model and mutators

---
+++ app/models/BaseModel.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use Carbon\Carbon;
+
+class BaseModel extends Eloquent {
+
+	public function getCreatedAtAttribute($value)
+	{
+	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+	    return $utc->setTimezone(Config::get('app.localTimezone'));
+	}
+
+
+	public function getUpdatedAtAttribute($value)
+	{
+	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+	    return $utc->setTimezone(Config::get('app.localTimezone'));
+	}
+
+
+}
