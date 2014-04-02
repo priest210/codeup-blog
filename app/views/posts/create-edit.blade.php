1d7ddf8fdbd4cebe615c4d64a3e9251bb9e2b303
Ken Priest <ken@priest210.com>
2014-04-02 18:44:21 -0500

    Added upload image, edit image and delete post functions.  Refactored code to add functions.

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -6,40 +6,52 @@
 	<div>
 @if (empty($post->id))
 			<h1 class="page-header">Create New Post</h1>
-			{{Form::open(array('action' => 'PostsController@store', 'class' => 'form-horizontal'))}}
+			{{Form::open(array('action' => 'PostsController@store', 'files' => true, 'class' => 'form-horizontal'))}}
 @else
 		  <fieldset>
 		    <legend>Update a blog entry</legend>
-		    {{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'class' => 'form-horizontal')) }}
+		    {{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
 @endif
 		    <div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
 		      {{ Form::label('title', 'Title', array('class' => 'col-lg-2 control-label')) }}
-		      <div class="col-lg-10">
+		      <div class="col-lg-6">
 		        {{ Form::text('title', null, array('class' => 'form-control', 'id' => 'title')) }}
 		        {{ $errors->first('title', '<p><span class="help-block">:message</span></p>') }}
 		      </div>
 		    </div>
 		    
-		    <div class="form-group">
+		    <div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
 		      <label for="body" class="col-lg-2 control-label">Body</label>
-		      <div class="col-lg-10">
+		      <div class="col-lg-6">
 		        {{ Form::textarea('body', null, array('class' => 'form-control', 'rows' => '5')) }}
 		        {{ $errors->has('body') ? $errors->first('body', '<p><span class="help-block">:message</span></p>') : '' }}
 		      </div>
 		    </div>
-		    
-		    <div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
-		    	{{ Form::label('body', 'Body', array('class' => 'col-lg-10 col-lg-offset-2'))}}
-		      <div class="col-lg-10 col-lg-offset-2">
-		    @if (empty($post->id))
-		        <button type="submit" class="btn btn-primary">Create Post</button>
-		    @else 
-		        <button type="submit" class="btn btn-primary">Update Post</button>
-		    @endif
-		      </div>
-		    </div>
+		    <div class="row">
+			   	<div class="col-lg-4" >
+		    		{{ Form::file('image') }}
+		    	</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="col-lg-4">	
+			    @if (empty($post->id))
+			        <button type="submit" class="btn btn-primary">Create Post</button>
+			    
+			    @else 
+			    <div>
+			        <button type="submit" class="btn btn-primary">Update Post</button>
+			    {{ Form::close() }}
+				</div>
+				<div>
+			    {{ Form::model($post, array('action' => array('PostsController@destroy', $post->id), 'type' => 'button', 'method' => 'delete')) }}
+			    	<button class="btn btn-danger">Delete Post</button>
+			   	{{ Form::close() }}
+			    </div>
+			    @endif
+				</div>
+			</div>
 		  </fieldset>
-		  {{ Form::close() }}
+
 	</div>
 @stop
 
