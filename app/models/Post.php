619e733ffc393f2d46716ad26e4e5e7f4c8edb6b
Ken Priest <ken@priest210.com>
2014-04-02 05:37:12 -0500

    Added user function to define relationship to user & getUpdatedAtAttribute function to format date/time.

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,9 +4,28 @@ class Post extends BaseModel {
 
 	protected $table = 'posts';
 
+	/**
+	* Define relationship to user (author)
+	*/
+
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
+
+	/**
+	 * Validation rules
+	 */
+
 	public static $rules = array(
 		'title' => 'required|max:100',
 		'body' => 'required|max:10000'
 	);
+
+	public function getUpdatedAtAttribute($value)
+	{
+	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+	    return $utc->setTimezone(Config::get('app.localTimezone'));
+	}
 }
 
