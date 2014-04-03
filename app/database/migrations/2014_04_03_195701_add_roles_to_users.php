3dde1c29a610e945382affbf11695f1b448491fb
Ken Priest <ken@priest210.com>
2014-04-03 16:14:31 -0500

    Initial commit.

---
+++ app/database/migrations/2014_04_03_195701_add_roles_to_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRolesToUsers extends Migration {
+
+	public function up()
+	{
+		Schema::table('users', function($table) {
+			$table->integer('role_id')->unsigned();
+			$table->foreign('role_id')->references('id')->on('roles');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table) {
+			$table->dropForeign('users_role_id_foreign');
+			$table->dropColumn('role_id');
+		});
+	}
+
+}
