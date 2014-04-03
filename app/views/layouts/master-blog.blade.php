1cd2097640984f4405d85ce54ce191248ababaea
Ken Priest <ken@priest210.com>
2014-04-03 16:08:05 -0500

    Added Disqus comment feature.

--- app/views/layouts/master-blog.blade.php
+++ app/views/layouts/master-blog.blade.php
@@ -65,6 +65,19 @@
 
     @yield('content')
 
+        <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'kenpriest'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    </script>
+
 </body>
     <p class="blog-post-title-meta">{{ $posts->links() }}</h>
 </div>
