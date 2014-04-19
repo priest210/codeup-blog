9880f2cf3c486e583c6ae12f118a085165ee36cc
Ken Priest <ken@priest210.com>
2014-04-19 14:00:42 -0500

    debug DB routing

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 		    'driver'    => 'mysql',
-		    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
-		    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
-		    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'codeup',
-		    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'password',
+		    'host'      => isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : 'localhost',
+		    'database'  => isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : 'blog_db',
+		    'username'  => isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : 'codeup',
+		    'password'  => isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : 'password',
 		    'charset'   => 'utf8',
 		    'collation' => 'utf8_unicode_ci',
 		    'prefix'    => '',
